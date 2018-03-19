b8861533405ab5de1b273cacaef0c4fa4d541e84
Lu Ma <windy005@126.com>
2018-03-19 15:12:21 -0500

    user process page table allocation complete --continue with Fork()

--- src/init.c
+++ src/init.c
@@ -5,10 +5,14 @@
 
 int main(int argc, char *argv[]) {
     printf("init process started! The pid is %d.\n", GetPid());
-    while (1) {
-        Pause();
-        printf("Now in init process. Pause is released once. The pid is %d.\n", GetPid());
-        ;
-    }
+    printf("Delay is called\n");
+    Delay(10);
+    printf("return to init process from delay");
+
+//    while (1) {
+//        Pause();
+//        printf("Now in init process. Pause is released once. The pid is %d.\n", GetPid());
+//        ;
+//    }
     return 0;
 }
